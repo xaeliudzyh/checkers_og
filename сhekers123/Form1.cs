63956d8d72331f34d9f4ad03ca72d9cd1e9d1fac
Георгий Цаголов <gtsagolov8@gmail.com>
2022-10-12 00:55:39 +0300

    added beautiful chekers' pieces, did a red cage when the cage is pressed, changed a color of the desk(not very bigchanges)

--- сhekers123/Form1.cs
+++ сhekers123/Form1.cs
@@ -13,7 +13,13 @@ namespace сhekers123
     public partial class Form1 : Form
     {
         const int mapSize = 8;   // length of the desk
-        const int cellSize = 55;   //size of the cage
+        const int cellSize = 100;   //size of the cage
+
+        int currentPlayer;
+
+        Button prevButton;
+
+        bool isMoving;
 
         int[,] map = new int[mapSize, mapSize];
 
@@ -23,8 +29,8 @@ namespace сhekers123
         public Form1()
         {
             InitializeComponent();
-            whiteFig = new Bitmap("w.png");     //adding a png for white figure
-            blackFig = new Bitmap("b.png");    // the same for black one
+            blackFig = new Bitmap(new Bitmap(@"C:\source\repos\сhekers123\b.png"), new Size(cellSize - 4, cellSize - 4));      //adding a png for white figure
+            whiteFig = new Bitmap(new Bitmap(@"C:\source\repos\сhekers123\w.png"), new Size(cellSize - 1, cellSize - 1));  // the same for black one
             this.Text = "Checkers";
 
             InIt();
@@ -32,12 +38,16 @@ namespace сhekers123
 
         public void InIt()
         {
+            currentPlayer = 1;
+            isMoving = false;
+            prevButton = null;
+
             map = new int[mapSize, mapSize] {
                 { 0,1,0,1,0,1,0,1 },
                 { 1,0,1,0,1,0,1,0 },
                 { 0,1,0,1,0,1,0,1 },
-                { 0,0,0,0,0,0,0,0 },
-                { 0,0,0,0,0,0,0,0 },
+                { 3,0,3,0,3,0,3,0 },
+                { 0,3,0,3,0,3,0,3 },
                 { 2,0,2,0,2,0,2,0 },
                 { 0,2,0,2,0,2,0,2 },
                 { 2,0,2,0,2,0,2,0 }
@@ -47,26 +57,80 @@ namespace сhekers123
         }
         public void CreateMap()
         {
-            this.Width= mapSize*cellSize;
-            this.Height= mapSize*cellSize;
+            this.Width = mapSize * cellSize;
+            this.Height = mapSize * cellSize;
 
-            for (int i = 0; i < mapSize; i++) 
+            for (int i = 0; i < mapSize; i++)
             {
-                for(int j=0; j < mapSize; j++)
+                for (int j = 0; j < mapSize; j++)
                 {
-                    
+
                     Button button = new Button();
-                    button.Location = new Point(j*cellSize,i*cellSize);
-                    button.Size= new Size(cellSize,cellSize);
+                    button.Location = new Point(j * cellSize, i * cellSize);
+                    button.Size = new Size(cellSize, cellSize);
+                    button.Click += new EventHandler(OnFigurePress);
                     if (map[i, j] == 1)
+                    {
                         button.BackgroundImage = whiteFig;   //adding whiteFig png on 1 in massive
+                        button.BackColor = Color.Gray;
+
+                    }
                     else
-                        if (map[i,j]==2)
+                    {
+                        if (map[i, j] == 2)
+                        {
                             button.BackgroundImage = blackFig;   //the same for blackFig png on 2 
+                            button.BackColor = Color.Gray;
+                        }
+                        else {
+                            if (map[i, j] == 3)
+                                button.BackColor = Color.Gray;
+                            else
+                                button.BackColor = Color.Linen;
+                        }
+                    }
+
                     this.Controls.Add(button);
                 }
+
+
+
             }
-        
         }
+        public void SwitchPlayer()
+        {
+            currentPlayer = currentPlayer == 1 ? 2 : 1;
+
+        }
+
+        public Color GetPrevButtonColor()
+        {
+            if ((prevButton.Location.Y / cellSize % 2) != 0) {
+                if ((prevButton.Location.X / cellSize % 2) == 0)
+                    return Color.Gray;
+            }
+            if ((prevButton.Location.Y / cellSize) % 2 == 0) {
+                if ((prevButton.Location.X / cellSize % 2) != 0)
+                    return Color.Gray;
+            }
+            return Color.White;
+        }
+
+        public void OnFigurePress(object sender, EventArgs e)
+        {
+            if (prevButton != null)
+                prevButton.BackColor = GetPrevButtonColor();
+
+            Button pressedButton = sender as Button;
+            if (map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] != 0 &&
+                map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] == currentPlayer) 
+            {
+                pressedButton.BackColor = Color.Red; 
+
+            }
+            prevButton = pressedButton;
+        }
+
     }
 }
+
