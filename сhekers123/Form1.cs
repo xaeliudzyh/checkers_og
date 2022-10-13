e0aece42e48d8d4607de2bac095b35f7194c29bb
Георгий Цаголов <gtsagolov8@gmail.com>
2022-10-13 21:21:59 +0300

    wrote a function that shows us if we have a possible eat step

--- сhekers123/Form1.cs
+++ сhekers123/Form1.cs
@@ -17,7 +17,11 @@ namespace сhekers123
 
         int currentPlayer;
 
-        Button prevButton;
+
+        int countEatSteps;     // number of "eatable" steps
+        Button prevButton;    //previous button
+        Button pressedButton;
+        bool isContinue = false;  //bool that shows us if we can continue step after eating 
 
         bool isMoving;
 
@@ -33,10 +37,10 @@ namespace сhekers123
             whiteFig = new Bitmap(new Bitmap(@"C:\source\repos\сhekers123\w.png"), new Size(cellSize - 1, cellSize - 1));  // the same for black one
             this.Text = "Checkers";
 
-            InIt();
+            Init();
         }
 
-        public void InIt()
+        public void Init()
         {
             currentPlayer = 1;
             isMoving = false;
@@ -46,8 +50,8 @@ namespace сhekers123
                 { 0,1,0,1,0,1,0,1 },
                 { 1,0,1,0,1,0,1,0 },
                 { 0,1,0,1,0,1,0,1 },
-                { 3,0,3,0,3,0,3,0 },
-                { 0,3,0,3,0,3,0,3 },
+                { 0,0,0,0,0,0,0,0 },
+                { 0,0,0,0,0,0,0,0 },
                 { 2,0,2,0,2,0,2,0 },
                 { 0,2,0,2,0,2,0,2 },
                 { 2,0,2,0,2,0,2,0 }
@@ -59,6 +63,7 @@ namespace сhekers123
         {
             this.Width = mapSize * cellSize;
             this.Height = mapSize * cellSize;
+            
 
             for (int i = 0; i < mapSize; i++)
             {
@@ -70,25 +75,13 @@ namespace сhekers123
                     button.Size = new Size(cellSize, cellSize);
                     button.Click += new EventHandler(OnFigurePress);
                     if (map[i, j] == 1)
-                    {
-                        button.BackgroundImage = whiteFig;   //adding whiteFig png on 1 in massive
-                        button.BackColor = Color.Gray;
-
-                    }
+                        button.Image = whiteFig;   //adding whiteFig png on 1 in massive
                     else
-                    {
                         if (map[i, j] == 2)
-                        {
-                            button.BackgroundImage = blackFig;   //the same for blackFig png on 2 
-                            button.BackColor = Color.Gray;
-                        }
-                        else {
-                            if (map[i, j] == 3)
-                                button.BackColor = Color.Gray;
-                            else
-                                button.BackColor = Color.Linen;
-                        }
-                    }
+                            button.Image = blackFig;   //the same for blackFig png on 2 
+
+                    button.BackColor = GetPrevButtonColor(button);
+
 
                     this.Controls.Add(button);
                 }
@@ -103,7 +96,7 @@ namespace сhekers123
 
         }
 
-        public Color GetPrevButtonColor()
+        public Color GetPrevButtonColor(Button prevButton)
         {
             if ((prevButton.Location.Y / cellSize % 2) != 0) {
                 if ((prevButton.Location.X / cellSize % 2) == 0)
@@ -113,22 +106,210 @@ namespace сhekers123
                 if ((prevButton.Location.X / cellSize % 2) != 0)
                     return Color.Gray;
             }
-            return Color.White;
+            return Color.Linen ;
         }
 
         public void OnFigurePress(object sender, EventArgs e)
         {
             if (prevButton != null)
-                prevButton.BackColor = GetPrevButtonColor();
+                prevButton.BackColor = GetPrevButtonColor(prevButton);
 
             Button pressedButton = sender as Button;
             if (map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] != 0 &&
                 map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] == currentPlayer) 
             {
-                pressedButton.BackColor = Color.Red; 
+                pressedButton.BackColor = Color.Red;
+                isMoving = true;
 
             }
+            else
+            {
+                if (isMoving)
+                {
+                    int temp = map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize];
+                    map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] = map[prevButton.Location.Y / cellSize, prevButton.Location.X / cellSize];
+                    map[prevButton.Location.Y / cellSize, prevButton.Location.X / cellSize]=temp;
+                    pressedButton.Image = prevButton.Image;
+                    prevButton.Image = null;
+                    isMoving = false;
+                }
+            }
+                 
             prevButton = pressedButton;
+        }
+
+         public bool IsFigureHasEatStep(int iCurrentFigure, int jCurrentFigure, bool isOneStep, int[] dir) //function that shows us if there is an enemy figure that can be eaten by us,
+                                                                                                           //and bool isOneStep shows us if the figure is the queen or just a simple figure
+        {
+            bool eatStep = false;
+            int j = jCurrentFigure + 1;
+            for(int i = iCurrentFigure; i >= 0; i--)
+            {
+                if (currentPlayer == 1 && isOneStep && !isContinue)      // figure is not a qoeen, its first step and player 1
+                    break;
+                if (dir[0] == 1 && dir[1] == -1 && !isOneStep)   //figure stepped down and left
+                    break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (map[i, j] != 0 && map[i, j] != currentPlayer)
+                    {
+                        eatStep = true;
+                        if (!IsInsideBorders(i - 1, j + 1))
+                            eatStep = false;
+                        else
+                        {
+                            if (map[i - 1, j + 1] != 0)
+                                eatStep = false;
+                            else
+                                return eatStep;
+                        }
+                    }
+                }
+                if (j < 7)
+                    j++;
+                else
+                    break;
+                if (isOneStep)
+                    break;
+            }
+
+
+            j = jCurrentFigure - 1;
+            for (int i = iCurrentFigure; i >= 0; i--)
+            {
+                if (currentPlayer == 1 && isOneStep && !isContinue)      // figure is not a qoeen, its first step and player 1
+                    break;
+                if (dir[0] == 1 && dir[1] == 1 && !isOneStep)   //figure stepped down and right
+                    break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (map[i, j] != 0 && map[i, j] != currentPlayer)
+                    {
+                        eatStep = true;
+                        if (!IsInsideBorders(i - 1, j - 1))
+                            eatStep = false;
+                        else
+                        {
+                            if (map[i - 1, j - 1] != 0)
+                                eatStep = false;
+                            else
+                                return eatStep;
+                        }
+                    }
+                }
+                if (j > 0 )
+                    j++;
+                else
+                    break;
+                if (isOneStep)
+                    break;
+            }
+
+
+
+            j=jCurrentFigure - 1;
+            for (int i = iCurrentFigure; i >= 0; i--)
+            {
+                if (currentPlayer == 2 && isOneStep && !isContinue)      // figure is not a qoeen, its first step and player 1
+                    break;
+                if (dir[0] == -1 && dir[1] == 1 && !isOneStep)   //figure stepped up and right
+                    break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (map[i, j] != 0 && map[i, j] != currentPlayer)
+                    {
+                        eatStep = true;
+                        if (!IsInsideBorders(i + 1, j - 1))
+                            eatStep = false;
+                        else
+                        {
+                            if (map[i + 1, j - 1] != 0)
+                                eatStep = false;
+                            else
+                                return eatStep;
+                        }
+                    }
+                }
+                if (j > 0 )
+                    j++;
+                else
+                    break;
+                if (isOneStep)
+                    break;
+            }
+
+
+
+            j = jCurrentFigure + 1;
+            for (int i = iCurrentFigure; i >= 0; i--)
+            {
+                if (currentPlayer == 2 && isOneStep && !isContinue)      // figure is not a qoeen, its first step and player 1
+                    break;
+                if (dir[0] == -1 && dir[1] == -1 && !isOneStep)   //figure stepped up and left 
+                    break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (map[i, j] != 0 && map[i, j] != currentPlayer)
+                    {
+                        eatStep = true;
+                        if (!IsInsideBorders(i - 1, j - 1))
+                            eatStep = false;
+                        else
+                        {
+                            if (map[i - 1, j - 1] != 0)
+                                eatStep = false;
+                            else
+                                return eatStep;
+                        }
+                    }
+                }
+                if (j > 0)
+                    j++;
+                else
+                    break;
+                if (isOneStep)
+                    break;
+            }
+
+            return eatStep;
+
+        }
+
+
+        public void CloseSteps()
+        {
+            for (int i = 0; i < mapSize; i++)
+                for (int j = 0; j < mapSize; j++)
+                    buttons[i, j] = GetPrevButtonColor(buttons[i, j]);
+        }
+
+        public bool IsInsideBorders(int ti, int tj)                            //checking if the choosen button inside the map
+        {
+            if (ti >= mapSize || tj >= mapSize || tj < 0 || ti < 0)
+                return false;
+            return true;
+        }
+
+        public void ActivateAllButtons()                                               //function that turns on all buttons
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    buttons[i, j].Enabled = true;
+                }
+            }
+        }
+
+        public void DeactivateAllButtons()                                         //function that turns off all buttons
+        {
+            for(int i = 0; i < mapSize; i++)
+            {
+                for(int j = 0; j < mapSize; j++)
+                {
+                    button[i, j].Enabled = false;
+                }
+            }
         }
 
     }
